6b0dd9f69432e8256874d9454dedd067ac2bd4aa
chienyulin0913 <83235561+chienyulin0913@users.noreply.github.com>
2024-08-07 10:35:04 -0400

    Create download_and_extract_dataset.sh

---
+++ datasets/download_and_extract_dataset.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+FILE=$1
+
+# List of available datasets
+AVAILABLE_DATASETS=("Boston" "tiles2")
+
+# Check if the specified dataset is available
+if [[ ! " ${AVAILABLE_DATASETS[@]} " =~ " ${FILE} " ]]; then
+  echo "Available datasets are Boston, tiles2"
+  exit 1
+fi
+
+echo "Specified [$FILE]"
+
+# URL map (add your direct download links here)
+declare -A URL_MAP
+URL_MAP["Boston"]="https://drive.google.com/uc?id=FILE_ID_BOSTON"
+URL_MAP["tiles2"]="https://drive.google.com/uc?id=FILE_ID_TILES"
+
+# Get the URL for the specified dataset
+URL=${URL_MAP[$FILE]}
+
+TAR_FILE=./datasets/$FILE.tar.gz
+TARGET_DIR=./datasets/$FILE/
+
+# Download the .tar.gz file
+wget -N $URL -O $TAR_FILE
+
+# Create the target directory if it does not exist
+mkdir -p $TARGET_DIR
+
+# Extract the .tar.gz file
+tar -zxvf $TAR_FILE -C ./datasets/
+
+# Remove the .tar.gz file after extraction
+rm $TAR_FILE
+
+echo "Dataset [$FILE] has been downloaded and extracted to $TARGET_DIR"
